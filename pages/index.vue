94c40a5adffa4818a829af7afc26b31bd2b19bf8
TheGhost <100290043+SebastianGT2003@users.noreply.github.com>
2023-09-14 19:45:44 -0500

    Add files via upload

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,71 @@
+
 <template>
+  
   <div>
-    <center>
-      <h1>Bienvenidos a la app de tareas</h1>
-      <p>Esta es una aplicación que permite gestionar las tareas de los estudiantes</p>
+    <v-img
+      class="mx-auto my-6"
+      max-width="228"
+      src="https://images.pexels.com/photos/344775/pexels-photo-344775.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    ></v-img>
 
-      <a href="/tareas">Click para iniciar (sin nuxtLink)</a>
-<br>
-<br>
-      <NuxtLink to="/tareas">
-        Click para iniciar (Con nuxtLink)
+    <v-card
+      class="mx-auto pa-12 pb-8"
+      elevation="8"
+      max-width="448"
+      rounded="lg"
+    >
+      <div class="text-center "><h1>Iniciar sesion</h1></div>
+      <div class="text-subtitle-1 text-medium-emphasis">Cuenta</div>
+
+      <v-text-field
+        density="compact"
+        placeholder="Correo electronico"
+        prepend-inner-icon="mdi-email-outline"
+        variant="outlined"
+      ></v-text-field>
+
+      <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
+        Contraseña
+      </div>
+
+      <v-text-field
+        :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+        :type="visible ? 'text' : 'password'"
+        density="compact"
+        placeholder="Ingrese su contraseña"
+        prepend-inner-icon="mdi-lock-outline"
+        variant="outlined"
+        @click:append-inner="visible = !visible"
+      ></v-text-field>
+
+      <v-btn
+        block
+        class="mb-8"
+        color="blue"
+        size="large"
+        variant="tonal"
+      >
+        Iniciar sesion
+      </v-btn>
+
+      <v-card-text class="text-center">
+        <NuxtLink to="/registro">
+          Registrarse ahora <v-icon icon="mdi-chevron-right"></v-icon>
       </NuxtLink>
-    </center>
-
+        
+       
+      </v-card-text>
+    </v-card>
   </div>
 </template>
 <script>
-definePageMeta({
+  export default {
+    data: () => ({
+      visible: false,
+    }),
+  }
+  definePageMeta({
   layout: "blank",
 });
+
 </script>
---
+++ pages/registro.vue
@@ -0,0 +1,112 @@
+
+<template>
+  
+  <div>
+    <v-img
+      class="mx-auto my-6"
+      max-width="228"
+      src="https://images.pexels.com/photos/344775/pexels-photo-344775.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    ></v-img>
+    
+    <v-card
+      class="mx-auto pa-12 pb-8"
+      elevation="8"
+      max-width="448"
+      rounded="lg"
+    >
+    <div class="text-center "><h1>Registrarse</h1></div>
+   
+    <form action="javascript:void(0)" @submit="">
+      <div class="text-subtitle-1 text-medium-emphasis">Nombre</div>
+
+      <v-text-field
+        density="compact"
+        v-model="user.name"
+        required
+        placeholder="Nombre"
+        prepend-inner-icon="mdi-email-outline" 
+        variant="outlined"
+      ></v-text-field>
+      <div class="text-subtitle-1 text-medium-emphasis">Correo electronico</div>
+      <v-text-field
+        density="compact"
+        v-model="user.correo"
+        required
+        placeholder="Correo electronico"
+        prepend-inner-icon="mdi-email-outline" 
+        variant="outlined"
+      ></v-text-field>
+
+      <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
+        Contraseña
+      </div>
+
+      <v-text-field
+        :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+        :type="visible ? 'text' : 'password'"
+        density="compact"
+        v-model="user.contraseña"
+        required
+        placeholder="Ingresa la contraseña"
+        prepend-inner-icon="mdi-lock-outline"
+        variant="outlined"
+        @click:append-inner="visible = !visible"
+      ></v-text-field>
+
+      <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
+        Confirmar contraseña
+      </div>
+
+      <v-text-field
+        :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+        :type="visible ? 'text' : 'password'"
+        density="compact"
+        v-model="user.confir_contraseña"
+        required
+        placeholder="Enter your password"
+        prepend-inner-icon="mdi-lock-outline"
+        variant="outlined"
+        @click:append-inner="visible = !visible"
+      ></v-text-field>
+
+      <v-btn
+        
+        block
+        class="mb-8"
+        color="blue"
+        size="large"
+        variant="tonal"
+      >
+        Registrarse
+      </v-btn>
+      
+    </form>
+      
+      <v-btn
+        
+        block
+        class="mb-8"
+        color="blue"
+        size="large"
+        variant="tonal"
+      >
+      <NuxtLink to="/">
+          Atras
+      </NuxtLink>
+      </v-btn>
+      
+    </v-card>
+  </div>
+</template>
+<script>
+const user = ref({ })
+  export default {
+    data: () => ({
+      visible: false,
+    }),
+  }
+  definePageMeta({
+  layout: "blank",
+});
+
+</script>
